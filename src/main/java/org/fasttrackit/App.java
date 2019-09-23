c62eba7a0ff319e8e126c5281a5fb6bcb0149467
Botan Daniela <botandaniela70@yahoo.com>
2019-09-23 19:27:45 +0300

    solved CozaLozaWoza with method

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        CozaLozaWoza cozaLozaWoza = new CozaLozaWoza();
+        cozaLozaWoza.printCozaLozaWoza(35,200);
+
+    }
+
+}
