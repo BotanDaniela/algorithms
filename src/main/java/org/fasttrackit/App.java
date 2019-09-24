e6a365c0570f9a92c7613a48c81845d6d6f6f9d8
Botan Daniela <botandaniela70@yahoo.com>
2019-09-24 23:11:00 +0300

    exercises Loop for

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,26 @@ public class App {
     public static void main(String[] args) {
         CozaLozaWoza cozaLozaWoza = new CozaLozaWoza();
         cozaLozaWoza.printCozaLozaWoza(35,200);
+        ExercisesLoopFor exercisesLoopFor = new ExercisesLoopFor();
+       // exercitiu 1
+        exercisesLoopFor.Numbers();
+        //exercitiul 2
+        exercisesLoopFor.Numbers2();
+        //exercitiul 3
+        exercisesLoopFor.Numbers3();
+        //exercitiul4
+        exercisesLoopFor.Numbers4();
+        //exercitiul 5
+        exercisesLoopFor.EvenNumbers();
+        //exercitiul 6
+        exercisesLoopFor.OddNumbers();
+        //exercitiul 7
+        exercisesLoopFor.SumNumbers();
+        //exercitiul 8
+        exercisesLoopFor.MediaNumbers();
+        //exercitiul 9
+        exercisesLoopFor.Array(new  String[][]{{"*******"},{"******"},{"*****"},{"****"},{"***"},{"**"},{"*"}});
+
 
     }
 
